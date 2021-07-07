b29d0f7832ba338130866ae3906fe70e3a14308a
Priyanshu Gupta <priyanshupankaj2001@gmail.com>
2021-07-07 21:00:04 +0530

    Final commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+
+router.get("/top", getTopProducts);
+
+// router.route("/:id").get(getProductById);
+
+router.route("/:id/reviews").post(protect, createProductReview);
+
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+export default router;
